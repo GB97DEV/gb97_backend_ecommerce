594d2178534e99d76812c8d73de9c97ae1f82ea9
John Ango <johnango@MacBook-Pro-de-John.local>
2023-08-16 16:17:32 -0500

    Example

---
+++ src/services/Example/src/functions/Crud/DeleteHandler.ts
@@ -0,0 +1,12 @@
+import Position from "../../models/PositionModel";
+
+import { deleteGeneric } from "../../../../../helpers/controllers/deleteGenericController";
+import { authMiddleware } from "../../../../../middleware/authentication";
+
+export const main = authMiddleware(async (event, context) => {
+  const acceptLanguage =
+    event.headers["Accept-Language"] ||
+    event.headers["accept-language"] ||
+    "es";
+  return deleteGeneric(Position, event, context, acceptLanguage);
+});
---
+++ src/services/Example/src/functions/Crud/UpdateHandler.ts
@@ -0,0 +1,12 @@
+import Position from "../../models/PositionModel";
+
+import { updateGeneric } from "../../../../../helpers/controllers/updateGenericController";
+import { authMiddleware } from "../../../../../middleware/authentication";
+
+export const main = authMiddleware( async (event, context) => {
+  const acceptLanguage =
+    event.headers["Accept-Language"] ||
+    event.headers["accept-language"] ||
+    "es";
+  return updateGeneric(Position, event, context, acceptLanguage);
+});
